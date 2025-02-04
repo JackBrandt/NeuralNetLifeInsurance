5d8d29317af2074057bb6828c110b607e624e410
Jack Brandt <jbrandt4@zagmail.gonzaga.edu>
2025-02-04 11:08:00 -0800

    Basic actuarial functions

---
+++ actu.py
@@ -0,0 +1,50 @@
+def payout_pv(fv, n, i):
+    '''Calculates the present value of a payment in n years at a given interest rate
+    Args:
+        fv: The final payment amount
+        n: Number of years til payout
+        i: Interest/Yield rate enter as a percentage (e.g., for 5% enter 5)
+    Returns:
+        Present value of payment
+    '''
+    return fv*((1+i/100)**-n)
+
+#print(payout_pv(100,7,10))
+
+def annuity_pmt(n,i,pv):
+    '''Calculates the payment for a annuity with a given duration, yield, and
+    present value
+    Args:
+        n: years duration
+        i: Interest/Yield rate enter as a percentage (e.g., for 5% enter 5)
+    pv: Present value
+    Returns:
+        Payment amount
+    '''
+    i=i/100
+    return pv/((1-(1+i)**-n)/i)
+
+#print(annuity_pmt(10,5,1000))
+
+def life_liability_pv(fv,i,mort_tab, defer_yrs=0):
+    '''Calculates the expected pv of life insurance policy liability
+    Args:
+        fv: payment amount on death
+        i: Interest/Yield rate enter as a percentage (e.g., for 5% enter 5)
+        mort_table: The odds of them dying in each year
+        defer_yrs: Number of years until policy start (e.g., if they are 20,
+          but we know they won't die until 25 at the soonest, then defer_yrs=4)
+    Returns:
+        expected pv of life insurance policy liability
+    '''
+    pv=0
+    for n,mort in enumerate(mort_tab,1+defer_yrs):
+        pv+=payout_pv(fv,n,i)*mort
+    return pv
+
+#print(life_liability_pv(100,10,[.5,.5]))
+
+# NOTE: With just life_liability_pv() we can calculate how much to charge for a life
+#   insurance policy *if* the customer wants to do a lump sum payment instead of
+#   an annuity
+
