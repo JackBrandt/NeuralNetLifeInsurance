fix softmax probability

neural_net.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
@@ -134,11 +135,13 @@ class NeuralNet(nn.Module):
         # Get model predictions
         self.eval()
         with torch.no_grad():
-            output = self(tensor_input)
-        output = pd.DataFrame(output.numpy())
+            logits = self(tensor_input)
+            probs = F.softmax(logits, dim=-1)
+        output = pd.DataFrame(probs.numpy())
+
+        # label rows
         output = output.transpose()
         output.index=[str(i) for i in range(25,121)]
-        #output.to_csv()
         return output
 
 if __name__ == "__main__":
@@ -160,8 +163,10 @@ if __name__ == "__main__":
     mort_df = model.get_life_data([[180,'m',72,130,'n','n',3,1,1,'n','n','n',4,'n',0,'n','n',200,'n','n','n','n','n']])
     plot_mort(mort_df)
     print(mort_df)
+    print(mort_df.sum())
     smoothed_df = gaussian_smooth(mort_df, sigma=5)
     plot_mort(smoothed_df)
     print(smoothed_df)
+    print(smoothed_df.sum())
     smoothed_df.to_csv('mortality.csv')
     #print(model.get_life_data())