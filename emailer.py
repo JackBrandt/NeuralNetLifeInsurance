f28a673cbeda4dec806976499399f4a19ff0fecf
Jack Brandt <jbrandt4@zagmail.gonzaga.edu>
2025-03-18 16:33:25 +0000

    emailer.py

---
+++ emailer.py
@@ -0,0 +1,29 @@
+# Import smtplib for the actual sending function
+import smtplib
+# Import the email modules we'll need
+from email.message import EmailMessage
+
+def send_email(address, subject, msg):
+    email = EmailMessage()
+    email.set_content(msg)
+    me='neuralnetlife@gmail.com'
+
+    email['Subject'] = subject
+    email['From'] = me
+    email['To'] = address
+
+    # Gmail SMTP settings
+    smtp_server = 'smtp.gmail.com'
+    port = 465  # SSL port
+
+    # Create secure SSL connection
+    with smtplib.SMTP_SSL(smtp_server, port) as s:
+        # Login with app password
+        password = input("Enter your email APP password: ")
+        s.login(me, password)
+
+        # Send the message
+        s.send_message(email)
+
+if __name__ == '__main__':
+    send_email('jbrandt4@zagmail.gonzaga.edu','Emailer Test','This is a test of the emailer.py')
