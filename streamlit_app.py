bff294ccf81d8d25a10082cf2a2eb2ddab5b53d2
Jack Brandt <jbrandt4@zagmail.gonzaga.edu>
2025-03-18 17:09:08 +0000

    Moved files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from neural_net import NeuralNet
 
-
+if not st.experimental_user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.experimental_user.name}!")
 
 life_insurance = st.Page("life_insurance_calculator.py", title="Neural Net Life Cost Predictor", icon='🧮')
 life_predictor = st.Page('life_predictor.py',title='Life Predictor', icon='🧮')
