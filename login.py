d298fc4c9e772d6ca034ff528d657a2305005fa3
jbaii <jiaxinbai830@gmail.com>
2025-04-03 14:19:00 -0700

    fixed login

--- login.py
+++ login.py
@@ -1,23 +1,20 @@
 import streamlit as st
 from cloud_storage import load_user_data, unload_user_data
-
-
+import os
+import subprocess as sp
 
 st.header('Welcome to Neural Net Life')
 st.subheader("Please login below, than navigate to any of the following pages on the left")
 
-if not st.experimental_user.is_logged_in:
+st.title("Login/Logout")
+st.write(st.experimental_user.get('email'))
+#st.switch_page("life_insurance_calculator.py")
+if st.experimental_user.get('email') is None:
     if st.button("Log in"):
         st.login()
-        user_data=load_user_data(st.experimental_user.email)
-        st.write(user_data)
 else:
-    user_data=load_user_data(st.experimental_user.email)
-    st.write(user_data)
-    st.write(f"Hello, {st.experimental_user.name}!")
-    st.write(st.experimental_user.items())
     if st.button("Log out"):
         st.logout()
-        user_data=unload_user_data(user_data)
-        st.write(user_data)
+
+
 
